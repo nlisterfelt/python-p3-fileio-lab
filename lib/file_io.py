07e1c8c026088c569649c123f38b6d02018a6154
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-09-10 12:59:51 -0500

    write append and read

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    new_name=str(file_name)+'.txt'
+    with open(new_name, mode='w', encoding='utf-8') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    new_name=str(file_name)+'.txt'
+    with open(new_name, mode='a', encoding='utf-8') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    new_name=str(file_name)+'.txt'
+    with open(new_name, encoding='utf-8') as text_file:
+        return text_file.read()
